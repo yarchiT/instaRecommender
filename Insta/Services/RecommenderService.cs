c378e95e44c943296e8034a9d53fd06494687fab
Yaroslav Tykhonchuk <Yaroslav.tykhonchuk@poort80.nl>
2019-04-18 14:15:45 +0300

    Fixed locationError

--- Insta/Services/RecommenderService.cs
+++ Insta/Services/RecommenderService.cs
@@ -33,6 +33,7 @@ namespace Insta.Services
            
            foreach (var locationDto in topCountriesList)
            {
+               if (locationDto.locationId != "")
                topLocationPostsList.AddRange(await scrapService.GetTopLocationPosts(locationDto.locationId));
            }
 
@@ -41,18 +42,19 @@ namespace Insta.Services
 
         private async Task<IEnumerable<Post>> GetTopHashtagPosts(IScrapService scrapService)
         {
-            var topCountriesList = new List<string>();
+            var topHashtagList = new List<string>();
             var topHashtagPostsList = new List<Post>();
             if (topLocationCountries.Count > 0)
             {
                 var allCountriesList = allHashtags.ToList(); 
                 allCountriesList.Sort((ht1,ht2) => ht1.Value.CompareTo(ht2.Value));
-                topCountriesList.AddRange(allCountriesList.Select(kv => kv.Key).ToList().Take(5));
+                topHashtagList.AddRange(allCountriesList.Select(kv => kv.Key).ToList().Take(5));
             }
            
-           foreach (var countryName in topCountriesList)
+           foreach (var hashtag in topHashtagList)
            {
-               topHashtagPostsList.AddRange(await scrapService.GetHashtagPosts(countryName));
+               if (hashtag != null || hashtag != "")
+                    topHashtagPostsList.AddRange(await scrapService.GetHashtagPosts(hashtag));
            }
 
            return topHashtagPostsList;
@@ -73,6 +75,9 @@ namespace Insta.Services
                         allHashtags[hashtag] = 1;
                 }
 
+                if (post.LocationCountry == null || post.LocationCountry == "" || post.LocationId == null || post.LocationId == "")
+                    return;
+                    
                 LocationDto locationDto = new LocationDto
                 {
                     countryName = post.LocationCountry,
